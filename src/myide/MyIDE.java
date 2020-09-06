82b7ae8d6710fc9fb3d5872fe8e1fcfbc99952d8
Ramiro404 <ramirocruznavarro@hotmail.com>
2020-09-06 17:52:41 -0500

    Wokrs

--- src/myide/MyIDE.java
+++ src/myide/MyIDE.java
@@ -16,7 +16,7 @@ public class MyIDE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola mundo");
+        System.out.println("Mi IDE");
     }
      
 }
