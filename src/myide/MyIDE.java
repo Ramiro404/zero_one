2112ea58366936547fcf087ce071eff63afa9969
Ramiro404 <ramirocruznavarro@hotmail.com>
2020-09-06 18:25:54 -0500

    Works

--- src/myide/MyIDE.java
+++ src/myide/MyIDE.java
@@ -16,7 +16,7 @@ public class MyIDE {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Mi IDE");
+        System.out.println("Fuck you world");
     }
      
 }
